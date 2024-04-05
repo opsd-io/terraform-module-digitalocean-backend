01776fb3bf7786f5a79f51a4bff3c26d9ce2a1f5
Piotr Stawarski <piotr.stawarski@zdt.io>
2024-04-05 09:57:44 +0200

    vars

--- variables.tf
+++ variables.tf
@@ -1,12 +1,21 @@
-# variable "variable_name" {
-#	  description = "variable description"
-#   type        = number
-#   default     = 1
-# }
+variable "region" {
+  description = "The region where the resources resides."
+  type        = string
+}
 
-# variable "variable_password" {
-#	  description = "variable description"
-#   type        = string
-#   sensitive   = true
-#   default     = "abc"
-# }
+variable "name" {
+  description = "The name of the bucket."
+  type        = string
+}
+
+variable "versioning" {
+  description = "Enable bucket versioning."
+  type        = bool
+  default     = true
+}
+
+variable "noncurrent_version_expiration" {
+  description = "Specifies the number of days after which an object's non-current versions expire."
+  type        = number
+  default     = 90
+}
