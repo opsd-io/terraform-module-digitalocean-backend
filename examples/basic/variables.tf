1e11c3033f8c703df3d21a8ca98269728a277f6c
Piotr Stawarski <piotr.stawarski@zdt.io>
2024-04-05 12:41:29 +0200

    examples

---
+++ examples/basic/variables.tf
@@ -0,0 +1,16 @@
+variable "digitalocean_token" {
+  description = "The  DigitalOcean API token. Defaults to the value of the DIGITALOCEAN_TOKEN / DIGITALOCEAN_ACCESS_TOKEN."
+  type        = string
+  default     = null
+}
+variable "spaces_access_id" {
+  description = "The access key ID used for Spaces API operations. Defaults to the value of the SPACES_ACCESS_KEY_ID."
+  type        = string
+  default     = null
+}
+
+variable "spaces_secret_key" {
+  description = "The secret access key used for Spaces API operations. Defaults to the value of the SPACES_SECRET_ACCESS_KEY."
+  type        = string
+  default     = null
+}
