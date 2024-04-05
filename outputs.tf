7d96de2a97cffbf40037a663fb8089be44c29472
Piotr Stawarski <piotr.stawarski@zdt.io>
2024-04-05 10:02:59 +0200

    out

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,24 @@
-# output "variable" {
-#   description = "output variable description"
-#   value       = variable.main.name
-# }
+output "region" {
+  description = "The region where the resources resides."
+  value       = digitalocean_spaces_bucket.state.region
+}
+
+output "name" {
+  description = "The name of the bucket."
+  value       = digitalocean_spaces_bucket.state.name
+}
+
+output "urn" {
+  description = "The uniform resource name of the bucket."
+  value       = digitalocean_spaces_bucket.state.urn
+}
+
+output "endpoint" {
+  description = "The FQDN of the bucket without the bucket name."
+  value       = digitalocean_spaces_bucket.state.endpoint
+}
+
+output "bucket_domain_name" {
+  description = "The FQDN of the bucket."
+  value       = digitalocean_spaces_bucket.state.bucket_domain_name
+}
